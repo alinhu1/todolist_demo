667a0e2d04baf62a64ac367352ec3f896de4e6f7
alinhu1 <alin.hu@idealjs.com>
2025-04-03 23:15:15 +0800

    docs:update todo list

--- app/components/Fetch.tsx
+++ app/components/Fetch.tsx
@@ -26,9 +26,29 @@ const Fetch = (props: IProps) => {
 
   const handleDelete = async (id: number) => {
     await fetch(`/api/todos/${id}`, {
+      headers: { 'Content-Type': 'application/json' },
       method: 'DELETE'
     })
     setTodo(todo.filter(props => props.id !== id))
+  }
+
+  const CompleteStatus = async (id: number) => {
+    const condition = todo.find(todo => todo.id === id)
+
+    const response = await fetch(`/api/todos/${id}`, {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'PUT',
+      body: JSON.stringify({
+        completed: !condition?.completed
+      })
+    })
+    const updateTodo = await response.json()
+    console.log(updateTodo);
+
+    setTodo(
+      todo.map((props) =>
+        props.id === id ? { ...props, completed: !props.completed } : props)
+    )
   }
 
 
@@ -37,8 +57,9 @@ const Fetch = (props: IProps) => {
       {todo.map((todo1) => (
         <li key={todo1.id}>
           {todo1.name}
-          <span className="todo_item">
-            {todo1.completed ? '√' : ' '}
+          <span className={`todo_item ${todo1.completed ? 'todo_item_active' : ''}`}
+            onClick={() => CompleteStatus(todo1.id)}
+          >
           </span>
           <button onClick={() => { handleDelete(todo1.id) }}>删除</button>
         </li>
