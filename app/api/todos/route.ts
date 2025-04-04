881f46aed01a764a7bc8aef29ea0c7581b2771c8
alinhu1 <alin.hu@idealjs.com>
2025-04-04 17:45:40 +0800

    docs:todo list zenstack get

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -1,7 +1,12 @@
 import { prisma } from "@/lib/prisma";
+import { PrismaClient } from "@prisma/client";
+import { enhance } from "@zenstackhq/runtime";
 
 export async function GET() {
-    const result = await prisma.todos.findMany()
+    const prisma = new PrismaClient();
+    const enhancePrisma = enhance(prisma)
+
+    const result = await enhancePrisma.todos.findMany() 
 
     return Response.json(result)
 
